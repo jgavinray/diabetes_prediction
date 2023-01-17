62399b4a5e86f20f75d98705cb56e26404f9da92
J. Gavin Ray <git@jgavinray.com>
2023-01-16 19:45:16 -0800

    Initial commit
    Signed-off-by: J. Gavin Ray <git@jgavinray.com>

---
+++ create_model.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+# Read data into pandas dataframe
+df=pd.read_csv(r'./diabetes.csv')
+#Define Feature Matrix (X) and Label Array (y)
+X=df.drop(['Outcome'],axis=1)
+y=df['Outcome']
+lr=RandomForestClassifier(n_estimators=53, n_jobs=1,random_state=8)
+lr.fit(X,y)
+
+#Serialize the model and save
+import joblib
+joblib.dump(lr, 'randomfs.pkl')
+print("Random Forest Model Saved")
+#Load the model
+lr = joblib.load('randomfs.pkl')
+# Save features from training
+rnd_columns = list(X.columns)
+joblib.dump(rnd_columns, 'rnd_columns.pkl')
+print("Random Forest Model Colums Saved")
